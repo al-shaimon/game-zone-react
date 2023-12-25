6f3089142bd396d76b91d2eb086db13b4b073f60
al-shaimon <alshaimon968@gmail.com>
2023-12-25 19:02:49 +0600

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h4 className='text-4xl'>Hello</h4>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <div>
+      <h2>THis is footer</h2>
+    </div>
+  );
+};
+
+export default Footer;
